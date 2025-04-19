5ef34318e1cf8a42e32d09dc71259f9f74ea1209
vinayaka <vinayakah47@gmail.com>
2025-04-19 20:10:38 +0530

    Web Assignment

---
+++ Day2/newFile.js
@@ -0,0 +1,23 @@
+var name ='Vinayaka'
+var age =21
+let gender='male'
+const institute='x-workz'
+document.writeln(age)
+
+let emp1={id:12,employeeName:'a',salary:1000}   /*declaring object or object creation*/
+
+let emp2 = new Object();      /*Instance*/
+emp2.id=13;
+emp2.employeeName='bb';
+emp2.salary=5000;
+emp2.id=14;
+
+function emp3(id,employeeName,salary){
+    this.id=id;
+    this.employeeName=employeeName;
+    this.salary=salary;
+    
+}
+
+let obj3 =emp3(satisfies,satisfies.s);
+
