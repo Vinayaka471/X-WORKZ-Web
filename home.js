dd5bb18970bd62f2479c6bc1f32109c66d283a05
vinayaka <vinayakah47@gmail.com>
2025-04-22 20:31:40 +0530

    sum.html

---
+++ home.js
@@ -0,0 +1,26 @@
+var names='vinayaka'
+names=1724
+names="Max"
+
+let names=172418
+names='Verstappen'
+names='Formula 1'
+
+const names='arjun'
+
+document.writeln(names)
+
+let emp1={id:15,employeeName:'Vinayaka',salary:40000}
+
+let emp2=new Object();
+emp2.id=15;
+emp2.employeeName='Vinayaka';
+emp2.salary=40000;
+
+function emp3(id,employeeName,salary){
+    this.id=id;
+    this.employeeName=employeeName;
+    this.salary=salary;
+}
+
+let obj3=emp3(satisfy,star,555)
