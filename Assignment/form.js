f2613e858e551cabeb57dcdcbbccf032a51391f3
vinayaka <vinayakah47@gmail.com>
2025-04-25 01:15:06 +0530

    Form Assignment

--- Assignment/form.js
+++ Assignment/form.js
@@ -1,4 +1,24 @@
-function submitForm(){
-    let name=document.forName.nameName.value;
-    alert(name);
+function onClick(){
+    alert("Sumitted")
+}
+function submitForm()
+{
+    let value=true;
+
+    let nameId = document.formName.nameName.value;
+    let numberId = document.formName.numberNumber.value;
+    let emailId = document.formName.emailEmail.value;
+    let genderId = document.formName.gender.value;
+    let checkId = document.formName.checkName.value;
+    let message = "Form Submitted Successfully!\n" +
+                  "Name: " + nameId + "\n" +
+                  "Number: " + numberId + "\n" +
+                  "Email: " + emailId + "\n"+ "Gender: "+genderId+
+                  "\n"+ checkId+"";
+
+    alert(message);
+    return value;
+}
+function clearForm(){
+    
 }
