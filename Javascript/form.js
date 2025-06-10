4b7d88e0de9c6d7e1c4c3b71d4421ae2470d36fc
vinayaka <vinayakah47@gmail.com>
2025-06-10 19:18:44 +0530

    Creating Form using HTML and JavaScript

--- Javascript/form.js
+++ Javascript/form.js
@@ -7,6 +7,11 @@ function validate() {
     let confirmPassword = document.formname.confirmConfirm.value;
 
 
+    const gender=document.querySelector('input[name="gender"]:checked');
+    const course=document.querySelector('input[name="course"]:checked');
+    const course1=Array.from(num).map(e=>e.value)
+
+
     if (names.length > 20) {
         val = false;
         alert('Form not submitted because name exceeds 20 characters.');
@@ -36,18 +41,34 @@ function validate() {
         alert('Form not submitted because passwords do not match.');
     }
 
-    const course1 = document.formname.course1.checked;
-    const course2 = document.formname.course2.checked;
-    const course3 = document.formname.course3.checked;
+  
 
     if (!course1 && !course2 && !course3) {
         val = false;
         alert('Form not submitted because no course was selected.');
     }
 
+    if(!gender) {
+        console.log(gender)
+        val=false;
+        alert('Please select gender')
+    }
+
+    if(course){
+
+
+        val=false;
+        alert('Please select course')
+    }
+    console.log(course1);
+    alert('stop here')
+    return val;
    
-    if (val === true) {
-        alert('Form submitted successfully!');
+    if (val === false) {
+        alert('Form not submitted ');
+    }
+    else{
+        alert('Form Submitted Successully')
     }
 
     return val;
